90509126077c4c43d116cc686ebc8c5a12b3fc34
TheEmber <petrusenko2004r@gmail.com>
2023-05-26 16:03:21 +0300

    Removed usings, loginmodel is now loginviewmodel

--- Models/TicketModel.cs
+++ Models/TicketModel.cs
@@ -1,11 +1,8 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace CourseWork.Models;
-    public class Ticket
+public class Ticket
     {
         [Key]
         public int Seat {get; set;}
